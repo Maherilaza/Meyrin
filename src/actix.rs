2f370a2a1032202efd7cde70dcd9a8804620970d
Maherilaza <maherilaza11@gmail.com>
2025-02-21 20:55:35 +0300

    feat(log) add log support

--- src/actix.rs
+++ src/actix.rs
@@ -1,5 +1,5 @@
 use crate::{activate_404_handler, actix_handle, data::init_args};
-// use actix_files::Files;
+use actix_files::Files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 activate_404_handler!();
@@ -8,13 +8,16 @@ actix_handle!(index, "static/index.html");
 #[actix_web::main]
 pub async fn init_server() -> std::io::Result<()> {
     let (port, ip) = init_args();
+    env_logger::builder()
+        .filter(None, log::LevelFilter::Info)
+        .init();
 
     HttpServer::new(|| {
         App::new()
-            .default_service(web::route().to(not_found))
             .route("/", web::get().to(index))
-            //.service(Files::new("/", "static").show_files_listing())
+            .service(Files::new("/", "static").show_files_listing())
             .route("/index", web::get().to(index))
+            .default_service(web::route().to(not_found))
     })
     .bind(format!("{}:{}", ip, port))?
     .run()
