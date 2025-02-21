21405daecf0a36abfa2f8a8cc6759e9317c98c3d
Maherilaza <maherilaza11@gmail.com>
2025-02-21 21:12:42 +0300

    fix(static) : fixing static scope

--- src/actix.rs
+++ src/actix.rs
@@ -1,5 +1,5 @@
 use crate::{activate_404_handler, actix_handle, data::init_args};
-// use actix_files::Files;
+use actix_files::Files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 activate_404_handler!();
@@ -13,7 +13,7 @@ pub async fn init_server() -> std::io::Result<()> {
         App::new()
             .default_service(web::route().to(not_found))
             .route("/", web::get().to(index))
-            //.service(Files::new("/", "static").show_files_listing())
+            .service(Files::new("/static", "./static").index_file(include_str!(".config/404.html")))
             .route("/index", web::get().to(index))
     })
     .bind(format!("{}:{}", ip, port))?
